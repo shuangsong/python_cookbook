eb70d869b84e0ce075dc1eb765794b02a4626b5a
shuangsong <shuangsong6@gmail.com>
2016-04-13 14:54:00 -0400

    add more

--- sample.py
+++ sample.py
@@ -15,9 +15,9 @@ from collections import import defaultdict
 user_movie_rating = defaultdict(lambda : defaultdict(int))
 
 user_movie_rating["Alice"]["LOR1"] = 4
-user_movie_rating["Alice"]["LOR1"] = 4
-user_movie_rating["Alice"]["LOR1"] = 4
-user_movie_rating["Alice"]["LOR1"] = 4
-user_movie_rating["Alice"]["LOR1"] = 4
+user_movie_rating["Alice"]["LOR2"] = 5
+user_movie_rating["Alice"]["LOR3"] = 3
+user_movie_rating["Alice"]["SW1"] = 5
+user_movie_rating["Alice"]["SW2"] = 3
 
 print user_movie_rating
